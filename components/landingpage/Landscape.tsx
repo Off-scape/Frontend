0acea9843de332b31190e7c126c83ed04bd351b6
Soltan <mahmudovsoltan477@gmail.com>
2026-03-04 21:37:22 +0400

    feat: enhance layout responsiveness for Landscape component

--- components/landingpage/Landscape.tsx
+++ components/landingpage/Landscape.tsx
@@ -3,7 +3,7 @@ import LandscapeRight from "@/components/landscape/LandscapeRight"
 
 const Landscape = () => {
     return (
-        <section className="max-[1285px]:px-5  grid grid-cols-2 gap-24 max-[1100px]:gap-14 items-center justify-between mb-32">
+        <section className="max-[1285px]:px-5  grid grid-cols-2 max-md:grid-cols-1 gap-24 max-[1100px]:gap-14 max-[850px]:gap-8 max-md:gap-12 items-center justify-between mb-32 max-md:mb-20 ">
             <LandscapeLeft />
             <LandscapeRight />
         </section>
