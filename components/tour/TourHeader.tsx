cc80ab1a76f668b02aa301be91d8a55a3aaf4611
Soltan <mahmudovsoltan477@gmail.com>
2026-03-17 18:24:17 +0400

    feat: implement TourCalendar and TourFilter components with date selection and filtering functionality

--- components/tour/TourHeader.tsx
+++ components/tour/TourHeader.tsx
@@ -1,51 +1,22 @@
-"use client"
-
+import TourCalendar from "@/components/tour/TourCalendar";
+import TourFilter from "@/components/tour/TourFilter";
 import TourLocationIcon from "@/icons/TourLocationIcon";
-import { formatDate } from "@/utils/formatDate";
-import { useRef, useState } from "react";
-import { FaAngleDown, FaRegClock } from "react-icons/fa";
+
 
 const TourHeader = () => {
-  const [date, setDate] = useState("");
-  const [open, setOpen] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const toggleDatePicker = () => {
-    if (open) {
-      inputRef.current?.blur(); // bağlamaq üçün blur istifadə edə bilərik
-      setOpen(false);
-    } else {
-      inputRef.current?.showPicker();
-      setOpen(true);
-    }
-  };
-
   return (
     <div>
       <div className="h-13 w-48 flex items-center justify-center gap-2 text-[#142A12] text-2xl font-bold bg-[#ECECEC] rounded-xl mb-14">
         <TourLocationIcon /> Tədbirlər
       </div>
       <div className="flex items-center justify-between mb-12">
-        <h3>Yaxın vaxtda keçiriləcək turlar</h3>
-        <div>
-          <div
-            onClick={toggleDatePicker}
-            className="w-76 h-11 relative bg-[#0B3E35] rounded-xl flex items-center gap-2 text-white cursor-pointer px-6"
-          >
-            <FaRegClock width={2} />
-            <span className="flex-1">{formatDate(date)}</span>
-            <input
-              ref={inputRef}
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              onBlur={() => setOpen(false)} // input blur olunca toggle-i resetlə
-              className="w-0 h-0 mt-5 text-lg font-medium text-[#FFFFFF]"
-            />
-            <FaAngleDown style={{"rotate":open?"180deg":"0deg", "animation":"ease-in-out",'transition':" 100ms all"}} />
-          </div>
+        <h3 className="text-[#142A12] text-[32px] font-bold">Yaxın vaxtda keçiriləcək turlar</h3>
+        <div className="flex items-center gap-2.5">
+          <TourCalendar />
+          <TourFilter />
         </div>
       </div>
+      
     </div>
   );
 };
---
+++ icons/FilterIcon.tsx
@@ -0,0 +1,13 @@
+
+const FilterIcon = () => {
+    return (
+        <div>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 3H2L10 13V17L14 21V13L22 3Z" stroke="#142A12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+
+        </div>
+    )
+}
+
+export default FilterIcon
