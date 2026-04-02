f0e9cb55b181daa4aa17598da66a76c75e3059d8
Soltan <mahmudovsoltan477@gmail.com>
2026-04-02 21:18:16 +0400

    Refactor code structure for improved readability and maintainability

---
+++ components/profile/Notification.tsx
@@ -0,0 +1,12 @@
+import { IoIosNotificationsOutline } from "react-icons/io";
+
+const Notification = () => {
+  return (
+    <div className="cursor-pointer flex flex-col items-center gap-1 text-white text-[18px] font-medium">
+      <IoIosNotificationsOutline size={28} color="white" />
+      Bildirişlər
+    </div>
+  )
+}
+
+export default Notification
