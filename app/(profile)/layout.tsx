28b078c711bf2f8e28a81dbf9b7c4a9d518da027
Soltan <mahmudovsoltan477@gmail.com>
2026-04-03 23:30:58 +0400

    feat: enhance ProfileLayout with sidebar toggle functionality and improve ProfileHeader layout

--- app/(profile)/layout.tsx
+++ app/(profile)/layout.tsx
@@ -1,17 +1,19 @@
 "use client"
 import ProfileHeader from "@/components/profile/ProfileHeader"
 import ProfileSideBar from "@/components/profile/ProfileSideBar"
+import { useState } from "react"
 
 export default function ProfileLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const [isOpen, setIsOpen] = useState(false)
   return (
     <main className=" min-h-screen  ">
-      <ProfileHeader />
-      <div className="max-w-7xl mx-auto flex  mt-45 gap-8 max-[1285px]:px-5 ">
-      <ProfileSideBar />
+      <ProfileHeader setIsOpen={setIsOpen} />
+      <div className="max-w-7xl mx-auto flex  lg:mt-45 mt-20  gap-8 max-[1285px]:px-5 ">
+      <ProfileSideBar isOpen={isOpen} setIsOpen={setIsOpen} />
       {children}
       </div>
     </main>
