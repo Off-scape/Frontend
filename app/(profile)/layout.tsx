8d075391ce72e195fc7c82942ee8c7b25ec9f9ae
Soltan <mahmudovsoltan477@gmail.com>
2026-04-03 21:31:48 +0400

    fix: improve layout spacing in ProfileLayout component

--- app/(profile)/layout.tsx
+++ app/(profile)/layout.tsx
@@ -8,9 +8,9 @@ export default function ProfileLayout({
   children: React.ReactNode
 }) {
   return (
-    <main className=" min-h-screen">
+    <main className=" min-h-screen  ">
       <ProfileHeader />
-      <div className="max-w-7xl mx-auto flex  mt-45 gap-8  ">
+      <div className="max-w-7xl mx-auto flex  mt-45 gap-8 max-[1285px]:px-5 ">
       <ProfileSideBar />
       {children}
       </div>
