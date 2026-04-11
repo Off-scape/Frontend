850992dc5815b4ef9841347184dd98f778a62cb9
cihankarimli <kerimlicihan1@gmail.com>
2026-04-11 23:43:53 +0400

    chenage PopularCategories gradient color

--- components/landingpage/PopularCategories.tsx
+++ components/landingpage/PopularCategories.tsx
@@ -2,8 +2,6 @@ import { categories } from "@/data/Categories";
 import Image from "next/image";
 
 export default function PopularCategories() {
-
-
   return (
     <section className="w-full max-w-7xl mx-auto px-4 py-16">
       <h2 className="text-4xl md:text-5xl font-bold text-center mb-12">
@@ -26,10 +24,9 @@ export default function PopularCategories() {
                 min-h-45
                 relative
                 z-10
-                
               `}
             >
-              <h3 className="text-3xl font-bold  text-gray-700 mx-2  leading-tight">
+              <h3 className="text-3xl font-bold text-gray-700 mx-2 leading-tight">
                 {category.title}
               </h3>
 
@@ -44,14 +41,15 @@ export default function PopularCategories() {
               </div>
             </div>
 
+            {/* Bottom Gradient Bar */}
             <div
               className={`
-                 relative bottom-3 left-0 right-0 h-3.75
-                bg-linear-to-b ${category.hoverGradient}
-                rounded-[20px]
-                opacity-30 group-hover:opacity-100
-                transform  scale-y-120 group-hover:scale-y-200 rounded-b-2xl
-                transition-all duration-600 ease-in-out
+                relative bottom-3 left-0 right-0 h-3.75
+                bg-gradient-to-r ${category.bottomGradient}
+                rounded-b-[20px]
+                opacity-40 group-hover:opacity-100
+                transform scale-y-[1.2] group-hover:scale-y-[2]
+                transition-all duration-500 ease-in-out
               `}
             />
           </div>
--- data/Categories.ts
+++ data/Categories.ts
@@ -1,30 +1,34 @@
-  export const categories = [
-    {
-      id: 1,
-      title: "Səyahət və təbiət",
-      icon: "/tebietImg.svg",
-      borderColor: "border-[#6A6A6D]",
-      hoverGradient: "from-[#699033] to-[#80C737]",
-    },
-    {
-      id: 2,
-      title: "Sosial Aktivliklər",
-      icon: "/socialImg.svg",
-      borderColor: "border-[#6A6A6D]",
-      hoverGradient: "from-[#EF8819] to-[#FC8C0C]",
-    },
-    {
-      id: 3,
-      title: "Hobilər və Tutqular",
-      icon: "/hobbyImg.svg",
-      borderColor: "border-[#6A6A6D]",
-      hoverGradient: "from-[#2D4872] via-[#E15609] to-[#FC8C0C]",
-    },
-    {
-      id: 4,
-      title: "Sağlam Həyat",
-      icon: "/lifeImg.svg",
-      borderColor: "border-[#6A6A6D]",
-      hoverGradient: "from-[#80C737] to-[#006738]",
-    },
-  ];
+export const categories = [
+  {
+    id: 1,
+    title: "Səyahət və təbiət",
+    icon: "/tebietImg.svg",
+    borderColor: "border-[#6A6A6D]",
+    hoverGradient: "from-[#699033] to-[#80C737]",
+    bottomGradient: "from-[#699033] to-[#80C737]",
+  },
+  {
+    id: 2,
+    title: "Sosial Aktivliklər",
+    icon: "/socialImg.svg",
+    borderColor: "border-[#6A6A6D]",
+    hoverGradient: "from-[#EF8819] to-[#FC8C0C]",
+    bottomGradient: "from-[#EF8819] to-[#FC8C0C]",
+  },
+  {
+    id: 3,
+    title: "Hobilər və Tutqular",
+    icon: "/hobbyImg.svg",
+    borderColor: "border-[#6A6A6D]",
+    hoverGradient: "from-[#2D4872] via-[#E15609] to-[#FC8C0C]",
+    bottomGradient: "from-[#2D4872] via-[#E15609] to-[#B1BA87]",
+  },
+  {
+    id: 4,
+    title: "Sağlam Həyat",
+    icon: "/lifeImg.svg",
+    borderColor: "border-[#6A6A6D]",
+    hoverGradient: "from-[#80C737] to-[#006738]",
+    bottomGradient: "from-[#80C737] to-[#006738]",
+  },
+];
