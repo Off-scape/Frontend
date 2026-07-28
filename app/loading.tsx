5d6aa46080e5517859570781ce67af39d49543e4
Soltan <mahmudovsoltan477@gmail.com>
2026-07-28 22:45:16 +0400

    feat: update loading component layout and add loading image for better user feedback

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+import LoadingImage from '@/public/common/assets/images/Loading.png'
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center ">
+    <div className="flex flex-col  h-screen items-center justify-center ">
+      <Image
+        src={LoadingImage}
+        alt="Loading"
+        fill
+      />  
+
       Loading...
     </div>
   );
